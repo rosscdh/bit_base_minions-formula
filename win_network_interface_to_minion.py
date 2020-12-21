c13b8f374001f806fe44de88cb1a24ac0282c3db
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2020-12-21 15:30:37 +0100

    helper scre

--- win_network_interface_to_minion.py
+++ win_network_interface_to_minion.py
@@ -1,13 +1,14 @@
 """
 convert the windows network_interfaces to a pillar
 
-salt '*' network.interfaces_names --output yaml
+salt '*' network.interfaces_names --output yaml > output.yaml
+python3 win_network_interface_to_minion.py
 """
 import yaml
 from pprint import pprint as pp
 from pathlib import Path
 
-data = yaml.load(Path('fixture-interface_names.yaml').read_bytes(), Loader=yaml.FullLoader)
+data = yaml.load(Path('output.yaml').read_bytes(), Loader=yaml.FullLoader)
 # print(data)
 output = dict(minion_hosts={})
 for item, data in data.items():
